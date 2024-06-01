0c6b7baad56618006446a693be8dc7becc1284d7
canwdev <c2097@live.com>
2024-06-01 14:26:59 +0800

    update: apps

--- frontend/src/apps/MediadevicesPlayer/utils/index.ts
+++ frontend/src/apps/MediadevicesPlayer/utils/index.ts
@@ -1,5 +1,7 @@
+import moment from 'moment/moment'
+
 export function snapVideoImageDownload(video) {
-  let canvas = document.createElement('canvas')
+  const canvas = document.createElement('canvas')
   canvas.width = video.videoWidth
   canvas.height = video.videoHeight
   canvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight)
@@ -7,7 +9,7 @@ export function snapVideoImageDownload(video) {
   // 将 canvas 转换为 png 格式并保存
   const dataUrl = canvas.toDataURL('image/png')
   const link = document.createElement('a')
-  link.download = 'video-screenshot.png'
+  link.download = `screenshot_${moment().format('YYYY-MM-DD_HH-mm-ss')}.png`
   link.href = dataUrl
   link.click()
 }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,7 +6,7 @@ import moment from 'moment/moment'
 // @ts-ignore
 const timeDisplay = BUILD_TIMESTAMP ? moment(BUILD_TIMESTAMP).format('YYYY-MM-DD HH:mm:ss') : 'N/A'
 console.info(
-  `%c ${pkg.name} ${import.meta.env.MODE} %c ${timeDisplay} %c`,
+  `%c ${pkg.name} ${pkg.version} ${import.meta.env.MODE} %c ${timeDisplay} %c`,
   'background:#258292; border-radius: 3px 0 0 3px; padding:2px 0; color: #f9f9f9; font-size: 10px;',
   'background:#f9f9f9; border-radius: 0 3px 3px 0; padding:2px 0; color: #258292; font-size: 10px; font-weight: bold;',
   'background:transparent',
